7a9f1150d4a826af80974b2ae684066d9ed80b3e
harshit1192 <harshit7404046572@gmail.com>
2026-06-21 15:04:30 +0530

    Add files via upload

---
+++ Datatype.js
@@ -0,0 +1,34 @@
+"use strict" // treat all js code as a new version of js 
+ let name = "Harshit Singh" // string data type
+ let age = "20" // number value
+ let isLoggedIn = false // boolean data type
+
+ // bigint for big number
+ // null => standalone value
+ // undefined
+// symbol (mostly use for unique)
+
+// type of
+console.log(typeof null);
+// conversion of data typers
+let Score = "88";
+let valueInNumber = Number(Score); 
+console.log(valueInNumber);
+
+let IsLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(IsLoggedIn); 
+console.log(booleanIsLoggedIn);//True
+
+let number = 45;
+let stringNumber = String(number);
+console.log(stringNumber);
+
+// operations
+let value = 3
+let negValue = -value
+console.log(negValue);
+// add two strings
+let str1 = "harshit"
+let str2 = "singh"
+let str3 = str1 + str2
+console.log(str3);
