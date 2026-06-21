5716cc0a04bdcde9eb86663e4ac886745001667b
harshit1192 <harshit7404046572@gmail.com>
2026-06-21 15:05:08 +0530

    Add files via upload

---
+++ Strings.js
@@ -0,0 +1,17 @@
+const name = "harshit"
+const repoCount = 50
+
+//console.log(name + repoCount + "Value"); this is old method
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+// Methods in strings
+
+//.length
+//.toUpperCase 
+//.toLowerCase
+//.CharAt(3)
+//.indexOf('t')
+//.subString(0 , 4);
+//.trim => to remove unwanted space
+
+
