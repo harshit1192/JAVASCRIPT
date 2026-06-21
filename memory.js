2dc66657add5c02b4b3ce468dff8eb95debcf4e1
harshit1192 <harshit7404046572@gmail.com>
2026-06-21 15:04:46 +0530

    Add files via upload

---
+++ memory.js
@@ -0,0 +1,10 @@
+//There are two types of memory
+// Stack(Primitive) , Heap(non-primitive)
+let myinstaname = "Madhav"
+let anothername = myinstaname
+anothername = "Harshit"
+
+console.log(myinstaname)
+console.log(anothername)
+
+
