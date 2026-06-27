b7d9e1176cf0b533baf4a46beebd5c54ff779ddd
harshit1192 <harshit7404046572@gmail.com>
2026-06-27 16:11:08 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,13 @@
+function sayMyName(){
+    console.log("y");
+    console.log("a");
+    console.log("s");
+    console.log("h");
+    
+}
+// sayMyName()
+
+function addTwoNumbers(number1 , number2){
+    console.log(number1 , number2);
+}
+addTwoNumbers(5,6);
